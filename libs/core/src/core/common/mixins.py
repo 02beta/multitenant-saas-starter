4b553407215a47c983fab280ef2f5ab85e7e6e39
Mac <mac@macanderson.com>
2025-08-15 11:13:18 -0700

    fix: github copilot pr comment regarding the timezone as arg issue in the mixins

--- libs/core/src/core/common/mixins.py
+++ libs/core/src/core/common/mixins.py
@@ -7,40 +7,18 @@ from uuid import UUID
 # from zoneinfo import ZoneInfo
 from sqlmodel import Field
 
-# def datetime_to_gmt_str(dt: datetime) -> str:
-#     """Convert datetime to GMT string format."""
-#     if not dt.tzinfo:
-#         dt = dt.replace(tzinfo=ZoneInfo("UTC"))
-
-#     return dt.strftime("%Y-%m-%dT%H:%M:%S%z")
-
-
-# class BaseModel(SQLModel):
-#     """Base model with custom datetime serialization."""
-
-#     def model_dump(self, **kwargs) -> dict:
-#         """Override model_dump to serialize datetime fields as GMT strings."""
-#         data = super().model_dump(**kwargs)
-
-#         # Convert datetime fields to GMT strings
-#         for key, value in data.items():
-#             if isinstance(value, datetime):
-#                 data[key] = datetime_to_gmt_str(value)
-
-#         return data
-
 
 class AuditFieldsMixin:
     """Mixin for created_at and updated_at timestamps."""
 
     created_at: datetime = Field(
-        default_factory=datetime.now,
+        default_factory=datetime.now(timezone.utc),
         nullable=False,
         title="Created at",
         description="The date and time the record was created",
     )
     updated_at: Optional[datetime] = Field(
-        default_factory=datetime.now,
+        default_factory=datetime.now(timezone.utc),
         nullable=False,
         title="Updated at",
         description="The date and time the record was last updated",
