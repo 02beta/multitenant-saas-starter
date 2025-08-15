b78fd668d7f1c6b2b71d5eddf84cad512876bd4b
Mac <mac@macanderson.com>
2025-08-15 09:09:22 -0700

    fix: added v1 api prefix to routes

--- apps/web/lib/constants.ts
+++ apps/web/lib/constants.ts
@@ -1,5 +1,5 @@
 export const apiUrl =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/v1/";
 
 export const appUrl =
   process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
